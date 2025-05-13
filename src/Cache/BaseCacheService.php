0cee90d467dd1c01854bb3ca4f408d0e4fc24c3e
antipda <antipda@ite-ng.ru>
2025-05-13 09:42:49 +0300

    Добавлены валидаторы, нормализаторы, различные генераторы и пр. Реализован метод put, чтобы наследник мог в частности добавить собственную логику к отправке данных в хранилище.

--- src/Cache/BaseCacheService.php
+++ src/Cache/BaseCacheService.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 namespace Demoniqus\CacheBundle\Cache;
 
+use Demoniqus\CacheBundle\Interfaces\Common\CacheKeyGeneratorInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\CacheMethodModelInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\CacheParamsNormalizerInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\CacheParamsValidatorInterface;
 use Demoniqus\CacheBundle\Interfaces\Common\CacheStorageInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\DataGeneratorInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\DataSerializerInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\DataTransmitterInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\OptionsResolverInterface;
 use Demoniqus\CacheBundle\ParamsBag\ParamsBagInterface;
 
 abstract class BaseCacheService implements CacheServiceInterface
@@ -26,53 +34,190 @@ abstract class BaseCacheService implements CacheServiceInterface
      * - getCacheKey() - параметры на основе ParamsBag
      */
     private string $key;
-    private string $cacheKeySplitter;
+
+    /**
+     * @var CacheParamsValidatorInterface[]
+     */
+    private array $validators = [];
+    /**
+     * @var CacheParamsNormalizerInterface[]
+     */
+    private array $normalizers = [];
+    private CacheKeyGeneratorInterface $cacheKeyGenerator;
+    /**
+     * @var OptionsResolverInterface[]
+     */
+    private array $optionsResolvers = [];
+    private DataGeneratorInterface $dataGenerator;
+
+    private ?DataSerializerInterface $dataSerializer = null;
+    private DataTransmitterInterface $dataTransmitter;
+
 
     public function __construct(
-        string                $key,
-        CacheStorageInterface $cacheStorage
+        string                     $key,
+        CacheStorageInterface      $cacheStorage,
+        CacheKeyGeneratorInterface $cacheKeyGenerator,
+        DataGeneratorInterface     $dataGenerator,
+        DataTransmitterInterface   $dataTransmitter
     )
     {
-
         $this->cacheStorage = $cacheStorage;
         $this->key = $key;
-        $this->cacheKeySplitter = ':';
+        $this->cacheKeyGenerator = $cacheKeyGenerator;
+        $this->dataGenerator = $dataGenerator;
+        $this->dataTransmitter = $dataTransmitter;
     }
 
-    public function update(?ParamsBagInterface $paramsBag): CacheServiceInterface
+    public function update(ParamsBagInterface $paramsBag)
     {
         //TODO Заложить возможность dependency
-        $this->cacheStorage->put(
-            $this->generateCacheKey($paramsBag),
-            $this->createData($paramsBag)
+        try {
 
-        );
+            $options = [];
+
+            $this
+                ->validateParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_UPDATE)
+                ->normalizeParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_UPDATE)
+                ->resolveOptions($paramsBag, CacheMethodModelInterface::CACHE_METHOD_UPDATE,$options);
+
+            $key = $this->generateCacheKey($paramsBag);
+            $data = $this->dataGenerator->generate($paramsBag);
+
+            $this->put(
+                $paramsBag,
+                $options,
+                $key,
+                $this->dataSerializer ?
+                    $this->dataSerializer->serialize($data) :
+                    $data
+            );
+
+            return $data;
+        } catch (\Throwable $e) {
+            //TODO Обработать ошибку и соо
+            return false;
+        }
+
+    }
+
+
+    protected function put(ParamsBagInterface $paramsBag, array $options, string $key, $data): void
+    {
+        $this->dataTransmitter->put($this->cacheStorage, $options, $key, $data);
+        //TODO Dependencies
+    }
+
+    public function get(ParamsBagInterface $paramsBag)
+    {
+        $this
+            ->validateParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_GET)
+            ->normalizeParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_GET)
+            ;
+        $data = $this->dataTransmitter->get($this->cacheStorage, $this->generateCacheKey($paramsBag));
+
+        return $this->dataSerializer && is_string($data) ? $this->dataSerializer->unserialize($data) : $data;
+    }
+
+    public function has(ParamsBagInterface $paramsBag): bool
+    {
+        return $this
+            ->validateParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_HAS)
+            ->normalizeParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_HAS)
+            ->cacheStorage
+            ->has($this->generateCacheKey($paramsBag));
+    }
+
+    public function delete(ParamsBagInterface $paramsBag): bool
+    {
+        try {//TODO dependency
+            $this
+                ->validateParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_DELETE)
+                ->normalizeParams($paramsBag, CacheMethodModelInterface::CACHE_METHOD_DELETE)
+                ->cacheStorage
+                ->delete($this->generateCacheKey($paramsBag));
+        } catch (\Throwable $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function generateCacheKey(ParamsBagInterface $paramsBag): string
+    {
+        return $this->cacheKeyGenerator->generate($this->key, $paramsBag);
+    }
+    protected function validateParams(ParamsBagInterface $paramsBag, string $action): self
+    {
+        foreach ($this->validators as $validator) {
+            $validator->validate($paramsBag, $action);
+        }
+
+        return $this;
+    }
+    protected function normalizeParams(ParamsBagInterface $paramsBag, string $action): self
+    {
+        foreach ($this->normalizers as $normalizer) {
+            $normalizer->normalize($paramsBag, $action);
+        }
+
+        return $this;
+    }
+    protected function resolveOptions(ParamsBagInterface $paramsBag, string $action, array &$options): self
+    {
+        foreach ($this->optionsResolvers as $optionsResolver) {
+            $optionsResolver->resolve($paramsBag, $action, $options);
+        }
 
         return $this;
     }
 
-    public function get(?ParamsBagInterface $paramsBag)
+    public function addValidator($validator): self
     {
-        return $this->cacheStorage->get($this->generateCacheKey($paramsBag));
-    }
+        /** @var CacheParamsValidatorInterface[] $validators */
+        $validators = is_array($validator) ? $validator : [$validator];
 
-    public function has(?ParamsBagInterface $paramsBag): bool
-    {
-        return $this->cacheStorage->has($this->generateCacheKey($paramsBag));
-    }
+        foreach ($validators as $validator) {
 
-    public function clear(?ParamsBagInterface $paramsBag): CacheServiceInterface
+            $this->validators[] = $validator;
+        }
+
+        return $this;
+    }
+    public function addNormalizer($normalizer): self
     {
-        $this->cacheStorage->put($this->generateCacheKey($paramsBag), null);
+        $normalizers = is_array($normalizer) ? $normalizer : [$normalizer];
+
+        foreach ($normalizers as $normalizer) {
+
+            $this->normalizers[] = $normalizer;
+        }
 
         return $this;
     }
 
-    abstract protected function getCacheKey(?ParamsBagInterface $paramsBag): string;
-    abstract protected function createData(?ParamsBagInterface $paramsBag);
-
-    private function generateCacheKey(?ParamsBagInterface $paramsBag): string
+    public function addOptionsResolver($resolver): self
     {
-        return $this->key . $this->cacheKeySplitter . $this->getCacheKey($paramsBag);
+        $resolvers = is_array($resolver) ? $resolver : [$resolver];
+
+        foreach ($resolvers as $resolver) {
+
+            $this->optionsResolvers[] = $resolver;
+        }
+
+        return $this;
+    }
+
+    public function setDataSerializer(DataSerializerInterface $dataSerializer): self
+    {
+        $this->dataSerializer = $dataSerializer;
+
+        return $this;
+    }
+
+    protected function getCacheStorage(): CacheStorageInterface
+    {
+        return $this->cacheStorage;
     }
 }
