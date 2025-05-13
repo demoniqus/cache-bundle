837f56a44a35f7d4dfeb640ea2faca34cd1536f9
antipda <antipda@ite-ng.ru>
2025-05-13 09:10:06 +0300

    Расширяем интерфейс в соответствии с возможностями Redis. Конкретизируем возвращаемые типы в переопределяемых методах.

--- src/Interfaces/Common/CacheStorageInterface.php
+++ src/Interfaces/Common/CacheStorageInterface.php
@@ -6,8 +6,12 @@ declare(strict_types=1);
 namespace Demoniqus\CacheBundle\Interfaces\Common;
 interface CacheStorageInterface extends CacheInterface
 {
+    public function eval(string $script, array $args = [], int $num_keys = 0);
 
+    public function hMSet(string $key, array $values, ?array $options): CacheStorageInterface;
+    public function hSet(string $key, string $member, $value): CacheStorageInterface;
 
-
+    public function beginTransaction(): CacheStorageInterface;
+    public function expire(string $key, int $timeout, ?string $mode = null): CacheStorageInterface;
 
 }
