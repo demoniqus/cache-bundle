cd1036ec9e2e882fe906c02343958af81b91b708
antipda <antipda@ite-ng.ru>
2025-05-13 09:30:51 +0300

    Расширил и обновил интерфейс. Аргумент типа ParamsBagInterface теперь является обязательным даже если в принципе дополнительных параметров нет.

--- src/Cache/CacheServiceInterface.php
+++ src/Cache/CacheServiceInterface.php
@@ -9,24 +9,29 @@ use Demoniqus\CacheBundle\ParamsBag\ParamsBagInterface;
 
 interface CacheServiceInterface
 {
-
-    public function update(?ParamsBagInterface $paramsBag): CacheServiceInterface;
+    /**
+     * @return mixed|false - return false in case cannot update cache
+     */
+    public function update(ParamsBagInterface $paramsBag);
 
     /**
      * Get value from storage by key
      *
      * @return mixed
      */
-    public function get(?ParamsBagInterface $paramsBag);
+    public function get(ParamsBagInterface $paramsBag);
 
     /**
      * Здесь можно проверять, существует ли такой ключ как непосредственно в хранилище, так и в памяти данного сервиса
      */
-    public function has(?ParamsBagInterface $paramsBag): bool;
+    public function has(ParamsBagInterface $paramsBag): bool;
 
     /**
      * Delete key from storage
      */
-    public function clear(?ParamsBagInterface $paramsBag): CacheServiceInterface;
+    public function delete(ParamsBagInterface $paramsBag): bool;
+
+
+    public function generateCacheKey(ParamsBagInterface $paramsBag): string;
 
 }
