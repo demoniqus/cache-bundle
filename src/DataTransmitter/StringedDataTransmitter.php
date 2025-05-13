a92a86fde8b4d379ace19110f6893cfcf9874c73
antipda <antipda@ite-ng.ru>
2025-05-13 09:26:13 +0300

    Сервисы обеспечивают передачу данных в хранилище и извлечение их оттуда: - HashedDataTransmitter  работает со структурами - StringedDataTransmitter работает с сериализованными строками

---
+++ src/DataTransmitter/StringedDataTransmitter.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Demoniqus\CacheBundle\DataTransmitter;
+
+use Demoniqus\CacheBundle\Interfaces\Common\CacheStorageInterface;
+use Demoniqus\CacheBundle\Interfaces\Common\DataTransmitterInterface;
+
+final class StringedDataTransmitter implements DataTransmitterInterface
+{
+
+    public function put(CacheStorageInterface $cacheStorage, array $options, string $key, $data): void
+    {
+        $cacheStorage->put(
+            $key,
+            $data,
+            $options
+        );
+    }
+
+    public function get(CacheStorageInterface $cacheStorage, string $key)
+    {
+        return $cacheStorage->get($key);
+    }
+}
